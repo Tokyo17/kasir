86049127e9090552e7acc5cd6a6757fc8e3d1eaf
mRezaMustofa <reza.mustofa@ist.id>
2023-08-04 15:31:27 +0700

    6

---
+++ app/api/auth/[...nextauth]/authOptiond.ts
@@ -0,0 +1,33 @@
+import type { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions={
+    session:{
+        strategy:'jwt'
+    },
+    // secret:process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+          // The name to display n the sign in form (e.g. "Sign in with...")
+          name: "Credentials",
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            console.log(credentials)
+            // Add logic here to look up the user from the credentials supplied
+            const user = { id: "1", name: credentials?.username, email: "jsmith@example.com" }
+      
+            if (user) {
+              // Any object returned will be saved in `user` property of the JWT
+              return user
+            } else {
+              // If you return null then an error will be displayed advising the user to check their details.
+              return null
+      
+            }
+          }
+        })
+      ]
+}
