8c75d80c6e9ac296a965c98e28e3ba4127172c1a
mRezaMustofa <reza.mustofa@ist.id>
2023-08-11 09:20:57 +0700

    add functin next5

--- app/MyContext.tsx
+++ app/MyContext.tsx
@@ -41,7 +41,38 @@ const MyContextProvider: React.FC<MyContextProviderProps> = ({ children }) => {
     // location.reload()
       audio.current?.pause()
       audio.current=new Audio(music?.url||'')
-
+      if ("mediaSession" in navigator) {
+        //     try{
+        //       navigator.mediaSession.metadata = new MediaMetadata({
+        //         title: music?.name,
+        //         artist:String( music?.index)+music?.name,
+        //         album: "The Ultimate Collection (Remastered)",
+        //       });
+        //       navigator.mediaSession.metadata.title="sadhsadb"
+        //     }catch(err){
+        //       console.log(err)
+        //     }
+        
+        
+        //     navigator.mediaSession.setActionHandler("previoustrack", () => {
+        //       // alert(JSON.stringify(dataMusic))
+        //       // console.log(music.index,dataMusic.length-1)
+        //       if(music.index>=1){
+        //         setMusic(dataMusic[music.index-1])
+        //       }
+        //       // setMusic()
+        //     });
+        
+        //     navigator.mediaSession.setActionHandler("nexttrack", () => {
+        //       if(music.index<=dataMusic.length-2){
+        //         // console.log(music.index,dataMusic.length-1)
+        //         setMusic(dataMusic[music.index+1])
+        //       }
+        //     });
+        }else{
+          console.log("media session not work")
+        }
+        
       audio.current.play()
       setIsPlaying(true)
       // console.log(navigator.mediaSession.metadata)
@@ -49,37 +80,6 @@ const MyContextProvider: React.FC<MyContextProviderProps> = ({ children }) => {
 
 
 
-  if ("mediaSession" in global.navigator) {
-//     try{
-//       navigator.mediaSession.metadata = new MediaMetadata({
-//         title: music?.name,
-//         artist:String( music?.index)+music?.name,
-//         album: "The Ultimate Collection (Remastered)",
-//       });
-//       navigator.mediaSession.metadata.title="sadhsadb"
-//     }catch(err){
-//       console.log(err)
-//     }
-
-
-//     navigator.mediaSession.setActionHandler("previoustrack", () => {
-//       // alert(JSON.stringify(dataMusic))
-//       // console.log(music.index,dataMusic.length-1)
-//       if(music.index>=1){
-//         setMusic(dataMusic[music.index-1])
-//       }
-//       // setMusic()
-//     });
-
-//     navigator.mediaSession.setActionHandler("nexttrack", () => {
-//       if(music.index<=dataMusic.length-2){
-//         // console.log(music.index,dataMusic.length-1)
-//         setMusic(dataMusic[music.index+1])
-//       }
-//     });
-}else{
-  console.log("media session not work")
-}
 
 // if ('setPositionState' in navigator.mediaSession) {
 //   if(audio.current){
